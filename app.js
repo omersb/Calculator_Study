add9dcd8d7c184160b955b3520bc9a1529d831b4
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-11 11:41:55 +0300

    js added

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 //* ======================================================
 //*                     IOS CALCULATOR
 //* ======================================================
+
+const prevDisp = document.querySelector(".previous-display");
+const currDisp = document.querySelector(".current-display");
+
+const btnContainer = document.querySelector(".buttons-container");
+
+let currOperand = ""
+let previousOperand = ""
+
+//! Butonları taşıyan container için event tanımlanması
+btnContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("num")) {
+        appendNumber(e.target.textContent);
+        updateDisplay();
+    }
+});
+
+const appendNumber = (num) => {
+    console.log(num);
+};
+
+const updateDisplay = () => {
+    currDisp.textContent = 
+};
