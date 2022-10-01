cefcf595c67f9cb638f749647f06602b023379e3
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-10-01 13:08:01 +0300

    update

--- app.js
+++ app.js
@@ -2,56 +2,56 @@
 //*                     IOS CALCULATOR
 //* =================================================
 //! Ekranlar
-const prevDisp = document.querySelector('.previous-display');
-const currDisp = document.querySelector('.current-display');
+const prevDisp = document.querySelector(".previous-display");
+const currDisp = document.querySelector(".current-display");
 
 //!Button container
-const btnContainer = document.querySelector('.buttons-container');
+const btnContainer = document.querySelector(".buttons-container");
 
 //! ara degerler icin degisken tanimlamalari
-let currOperand = '';
-let previousOperand = '';
-let operation = '';
+let currOperand = "";
+let previousOperand = "";
+let operation = "";
 
 let equalOrPercentPressed = false;
 
 //! Butonlari tasiyan container icin event tanimlamasi
-btnContainer.addEventListener('click', (e) => {
+btnContainer.addEventListener("click", (e) => {
   //! Herhangi bir sayi(num) sayiya tiklanildi ise
-  if (e.target.classList.contains('num')) {
+  if (e.target.classList.contains("num")) {
     appendNumber(e.target.textContent);
     updateDisplay();
   }
 
   //! Herhangi bir operator butonuna (+,-,x,/) tiklanildi ise
-  if (e.target.classList.contains('operator')) {
+  if (e.target.classList.contains("operator")) {
     chooseOperator(e.target.textContent);
     updateDisplay();
   }
   //! Esittir butonuna tiklanildi ise
-  if (e.target.classList.contains('equal')) {
+  if (e.target.classList.contains("equal")) {
     calculate();
     updateDisplay();
     equalOrPercentPressed = true;
   }
 
   //! AC butonuna tiklanildi ise
-  if (e.target.classList.contains('ac')) {
-    previousOperand = '';
-    currOperand = '';
-    operation = '';
+  if (e.target.classList.contains("ac")) {
+    previousOperand = "";
+    currOperand = "";
+    operation = "";
     updateDisplay();
   }
 
   //! PM butonuna tiklanildi ise
-  if (e.target.classList.contains('pm')) {
+  if (e.target.classList.contains("pm")) {
     if (!currOperand) return;
     currOperand *= -1;
     updateDisplay();
   }
 
   //! Percent butonuna tiklanildi ise
-  if (e.target.classList.contains('percent')) {
+  if (e.target.classList.contains("percent")) {
     if (!currOperand) return;
     currOperand = currOperand / 100;
     updateDisplay();
@@ -61,23 +61,23 @@ btnContainer.addEventListener('click', (e) => {
 
 const appendNumber = (num) => {
   //! Eger onceden 0 girilmisse ve tekrardan 0 girilise geri don
-  if (currOperand === '0' && num === '0') return;
+  if (currOperand === "0" && num === "0") return;
 
   //! Eğer ilk olarak 0 girilmisse ve sonrasinda da . haricinde baska
   //! bir sayi girilmis ise sadece girilen yeni sayiyi degiskene aktar.
   //! Orn: 09 => 9 , 03 => 3 , 0.1 => 0.1
-  if (currOperand === '0' && num !== '.') {
+  if (currOperand === "0" && num !== ".") {
     currOperand = num;
     return;
   }
 
   //! Eğer şu anki sayi . ise ve önceki girilen sayi . iceriyorsa geri don
-  if (num === '.' && currOperand.includes('.')) return;
-  
+  if (num === "." && currOperand.includes(".")) return;
+
   //! sayı sınırlandı
   if (currOperand.length > 10) return;
 
-  if (equalOrPercentPressed){
+  if (equalOrPercentPressed) {
     currOperand = num;
     equalOrPercentPressed = false;
     return;
@@ -104,7 +104,7 @@ const chooseOperator = (op) => {
   //! Degisken swapiing
   operation = op;
   previousOperand = currOperand;
-  currOperand = '';
+  currOperand = "";
 };
 
 const calculate = () => {
@@ -114,16 +114,16 @@ const calculate = () => {
   const current = Number(currOperand);
 
   switch (operation) {
-    case '+':
+    case "+":
       calculation = prev + current;
       break;
-    case '-':
+    case "-":
       calculation = prev - current;
       break;
-    case 'x':
+    case "x":
       calculation = prev * current;
       break;
-    case '÷':
+    case "÷":
       calculation = prev / current;
       break;
     default:
@@ -134,6 +134,6 @@ const calculate = () => {
 
   //! Esittir butonuna tiklanildiginda ekranda gozukmemesi icin
   //! previousOperand ve operation'ı silmemiz gerekir
-  previousOperand = '';
-  operation = '';
+  previousOperand = "";
+  operation = "";
 };
